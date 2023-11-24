6bc1b9f7c566000989a72d89085717ab452c02f1
virto <virtopeanu.dragos@yahoo.com>
2023-11-24 19:19:58 +0200

    implemented bottom navigation bar

--- HealthLog/app/src/main/java/com/example/healthlogh/MainActivity.java
+++ HealthLog/app/src/main/java/com/example/healthlogh/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.healthlogh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Load the default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+
+        //BottomNavigationBar
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int id = item.getItemId();
+            if (id == R.id.navigation_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.navigation_workout) {
+                selectedFragment = new WorkoutFragment();
+            } else if (id == R.id.navigation_food) {
+                selectedFragment = new FoodFragment();
+            } else if (id == R.id.navigation_progress) {
+                selectedFragment = new ProgressFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
+
     }
 }
