e2a39c9afaa247bcd16e14731431e28444336c27
virto <virtopeanu.dragos@yahoo.com>
2023-11-25 21:55:45 +0200

    added top bar and implemented drawer menu for top bar

--- HealthLog/app/src/main/java/com/example/healthlog/MainActivity.java
+++ HealthLog/app/src/main/java/com/example/healthlog/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.healthlog;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,41 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, new HomeFragment())
                 .commit();
+
+
+
+        // Initialize DrawerLayout and NavigationView
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.top_nav_view);
+
+        // Setup the Toolbar
+        Toolbar toolbar = findViewById(R.id.top_app_bar);
+        setSupportActionBar(toolbar);
+
+        // Setup Drawer Toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Setup Drawer Navigation Item Click Listener
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                // Handle the home action
+            } else if (id == R.id.nav_profile) {
+                // Handle the profile action
+            }
+
+            // Close drawer after item is selected
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
 
 
         //BottomNavigationBar
@@ -48,4 +90,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
